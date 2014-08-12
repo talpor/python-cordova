ea74db06355159477db8e9855c1347933209533e
Kristoffer Pantic <kpantic@talpor.com>
2014-08-12 16:01:38 -0430

    Improves the README file and some setup.py details for PyPi

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# Python Cordova
+===============================================
+python-cordova
+===============================================
 
 This is a very simple Python library to interface with the Cordova (Phonegap) command line tool.
+
+
+Key concepts
+===============================================
+- Interact with the Cordova CLI directly from Python
+- Enables building and archiving PhoneGap applications from your Python code
+
+
+Usage
+===============================================
+
+.. code-block:: python
+   import cordova
+
+   application = cordova.App(
+       'PhoneGap Application',
+       APPLICATION_ROOT
+   )
+
+   application.build('android') # or any installed platform
+   application.archive('ios') # or any installed platform
--- setup.py
+++ setup.py
@@ -3,25 +3,34 @@ from setuptools import setup
 
 from cordova import __version__
 
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
 setup(
     name="python-cordova",
     version=__version__,
     description="A python interface for Apache Cordova CLI",
+    long_description=README,
     author="Kristoffer Pantic",
-    author_email="me@kristofferpantic.com",
+    author_email="kpantic@talpor.com",
     url="http://github.com/talpor/python-cordova",
     packages=['cordova'],
-    download_url='https://github.com/talpor/python-cordova/tarball/0.1',
     classifiers=[
-        "Development Status :: 1 - Beta",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Topic :: Internet",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7"],
-    install_requires=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Localization',
+        'Topic :: Utilities',
     ],
+    keywords=['python', 'cordova', 'phonegap', 'cordova-cli']
 )
